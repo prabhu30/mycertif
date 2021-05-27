087ea73cccd8fbba865c8a38f1e42430a57ed86b
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2021-05-28 00:58:11 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function myFunction(){
 	alert("I look amazing on PC!");
 }
+
+function menubtn()
+{
+	let dropdownBtn = document.querySelector('.menu');
+	let menuContent = document.querySelector('.dropdown-menu');
+	let navcolor = document.querySelector('nav').style.backgroundColor;
+	if(menuContent.style.display===""){
+	   menuContent.style.display="block";
+	   menuContent.style.backgroundColor = navcolor;
+	} else {
+	   menuContent.style.display="";
+	}
+}
